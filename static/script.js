4411e7d62d0c85d13b6746b903518a0b5e7ce897
Luis Sánchez <sanchezle21@gmail.com>
2023-05-22 22:45:41 +0200

    movement constraints and increase height function

--- static/script.js
+++ static/script.js
@@ -7,99 +7,197 @@ document.addEventListener('DOMContentLoaded', function() {
   const timeDisplay = document.getElementById('time-display');
   const ball = document.getElementById('ball');
   const arrowButtons = document.getElementById('arrow-buttons');
-
+  const heightDisplay = document.getElementById('heightDisplay');
 
 
   let time = 60; // Set the initial time in seconds
   let score = 0; // Initialize the score variable
   let bottom = 0;
+  // jump variables
   let gravity = 0.9;
   let isJumping = false;
+  let currentHeight = 0;
+  let maxHeight = 399;
+  let heightTimerId;
+  // slide variables
   let isGoingLeft = false;
   let isGoingRight = false;
   let left = 0;
   let leftTimerId;
   let rightTimerId;
+  let movingTimerId;
+  
+  //update score function
+  function updateScore() {
+    score++;
+    scoreDisplay.textContent = `Score: ${score}`;
+  }
+  
+  // Update score function
+  function updateScore() {
+    score++;
+    scoreDisplay.textContent = `Score: ${score}`;
+  }
 
 
-  function jump(){
-    if(isJumping) return;
-    let timerId =setInterval(function(){
-      if(bottom > 400){
-      clearInterval(timerId)
-        let timerDownId = setInterval(function(){
-          if(bottom < 0){
-            clearInterval(timerDownId)
+ //increase height function
+   function increaseHeight() {
+
+    //stops the interval when the currentHeight is equal to the maxHeight and current height stops increasing
+    if (currentHeight >= maxHeight) {
+      currentHeight = maxHeight;
+      clearInterval(heightTimerId);  
+    } 
+    currentHeight += 20;
+    console.log("height: " + currentHeight);
+    // Update the height of the element
+    heightDisplay.style.height = currentHeight + 'px';
+    // Update the height display
+    heightDisplay.textContent = 0;
+    return currentHeight; 
+  }
+
+  function keydownHandler(event) {
+    if (event.keyCode === 32) { // Spacebar key code
+      clearInterval(heightTimerId);
+      heightTimerId = setInterval(increaseHeight, 20);
+      console.log("intervalId: " + heightTimerId);  
+    }
+    
+  }
+
+  function keyupHandler(event) {
+    if (event.keyCode === 32) {
+       // Spacebar key code
+      
+      jump();
+    }
+  }
+  document.addEventListener('keydown', keydownHandler);
+  document.addEventListener('keyup', keyupHandler);
+  
+  // why does the height keeps increassing after the spacebar is released?
+  // because the keydownHandler is still running
+  // how to stop the keydownHandler?
+  // use clearInterval to stop the interval
+  // how to stop the interval?
+  // where should i put the clearInterval?
+
+
+  function jump() {
+    clearInterval(heightTimerId); 
+    // Clear the interval when jump is called
+    // Your jump logic here;
+    //
+    if (isJumping) return;
+    let timerId = setInterval(function () {
+      if (bottom > currentHeight || bottom > 400) {
+        clearInterval(timerId);
+        let timerDownId = setInterval(function () {
+          if (bottom < 0) {
+            clearInterval(timerDownId);
             isJumping = false;
+            currentHeight = 0;
           }
-          bottom -= 10
+          bottom -= 10;
           dog.style.bottom = bottom + 'px';
+          heightDisplay.style.height = bottom + 'px';
         }, 20);
       }
       isJumping = true;
       bottom += 50;
       bottom = bottom * gravity;
-      console.log(bottom)
+      console.log(bottom);
       dog.style.bottom = bottom + 'px';
-      }, 20);
+    }, 20);
   }
+
  
-  function slideLeft(){
-      if (isGoingRight) {
-        clearInterval(rightTimerId);
-        isGoingRight = false;
-      }
-      isGoingLeft = true;
-      leftTimerId = setInterval(function(){
-        left -= 5; 
-        console.log('going left')
-        dog.style.left = left + 'px';
-      }, 20);
-  }
 
-  function slideRight(){
-      if (isGoingLeft) {
-        clearInterval(leftTimerId);
-        isGoingLeft = false;
-      }
-      isGoingRight = true;
-      rightTimerId = setInterval(function(){
-        left += 5;
-        console.log('going right')
-        dog.style.left = left + 'px';
-      }, 20);
-  }
-
-  function stopSliding(){
-      console.log('stop sliding')
-      clearInterval(leftTimerId);
+  function slideLeft() {
+    if (isGoingRight) {
       clearInterval(rightTimerId);
-      isGoingLeft = false;
       isGoingRight = false;
+    }
+    if (left > -105) {
+      clearInterval(movingTimerId); // Clear the previous moving timer
+      isGoingLeft = true;
+      movingTimerId = setInterval(function () {
+      left -= 5;
+      console.log('going left');
+        if (left < -105) {
+          left = -105;
+        }
+        dog.style.left = left + 'px';
+      }, 20);
+    }
+    
   }
-
-  function control(e){
-      if(e.keyCode === 32){
-        jump();
-    } else if(e.keyCode === 37){
-        slideLeft();//if you want to slide left
-    } else if(e.keyCode === 39){
-        slideRight();//if you want to slide right
-    } else if(e.keyCode === 40){
-      stopSliding();//if you want to stop sliding
+  
+  function slideRight() {
+    if (isGoingLeft) {
+      clearInterval(leftTimerId);
+      isGoingLeft = false;
+    }
+    if (left < 250) {
+      clearInterval(movingTimerId); // Clear the previous moving timer
+      isGoingRight = true;
+      movingTimerId = setInterval(function () {
+      left += 5;
+      console.log('going right');
+        if (left > 260) {
+          left = 260;
+        }
+        dog.style.left = left + 'px';
+      }, 20);
+    }
+  }
+  
+  function stopSliding() {
+    console.log('stop sliding');
+    clearInterval(movingTimerId); // Clear the current moving timer
+    isGoingLeft = false;
+    isGoingRight = false;
+  }
+  
+  // Event listener for the jump function and the arrow keys
+  function control(e) {
+    if (e.keyCode === 37) {
+      slideLeft(); // If you want to slide left
+    } else if (e.keyCode === 39) {
+      slideRight(); // If you want to slide right
+    } else if (e.keyCode === 40) {
+      stopSliding(); // If you want to stop sliding
+    } else if (e.keyCode === 83) {
+      console.log('s pressed');
+      updateScore(); // If you want to add score
     }
   }
   document.addEventListener('keydown', control);
-  // Event listener for the jump function
+  
+  document.querySelectorAll('.arrow-buttons').forEach(button => {
+    button.addEventListener('mousedown', (event) => {
+      const direction = event.target.getAttribute('id');
+      if (direction === "up") {
+        clearInterval(heightTimerId);
+        heightTimerId = setInterval(increaseHeight, 20);
+        console.log("intervalId: " + heightTimerId);
+      }
+    });
+  
+    button.addEventListener('mouseup', (event) => {
+      const direction = event.target.getAttribute('id');
+      if (direction === "up") {
+        jump();
+      }
+    });
+  });
   // create a function for arrows to triger jump, slide left, slide right functions.
   document.querySelectorAll('.arrow-button').forEach(button => {
     button.addEventListener('click', function(event) {
       const direction = event.target.getAttribute('id');
     
-        if (direction === "up") {
-        // call the jump function when the up arrow is clicked
-        jump(); 
-      } else if (direction === "down") {
+        if (direction === "down") {
         
           stopSliding();
 
@@ -212,7 +310,6 @@ document.addEventListener('DOMContentLoaded', function() {
       console.error('Error sending score to Flask:', error);
     });
   }
-
 
 });
 
